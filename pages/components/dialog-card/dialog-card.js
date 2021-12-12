3ae06779b330ea7c2c36dd36efa4bb7fab963359
Guanyu <im.dugy@foxmail.com>
2021-12-12 21:33:12 +0800

    给消息列表、Dialog 添加过度动画

--- pages/components/dialog-card/dialog-card.js
+++ pages/components/dialog-card/dialog-card.js
@@ -7,6 +7,18 @@ Component({
     showDialog: {
       type: Boolean,
       value: false
+    },
+    title: {
+      type: String,
+      value: '提示'
+    },
+    confirmText: {
+      type: String,
+      value: '确定'
+    },
+    cancelText: {
+      type: String,
+      value: '取消'
     }
   },
 
